e305f73c87804e0dd7da7cc388358ad666f49138
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-06-26 21:44:32 -0300

    Realizando likes nos comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,15 @@
 import styles from '../css/modules/Comment.module.css';
 import Trash from '../../src/assets/trash.svg?react';
 import { Avatar } from './Avatar';
+import React from 'react';
+
 export function Comment({ content, onDeleteComment }) {
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+  const [likeCount, setLikeCount] = React.useState(0);
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
   return (
     <div className={styles.comment}>
@@ -22,9 +28,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAACXBIWXMAAAsTAAALEwEAmpwYAAABN0lEQVR4nM2UPUsDQRCGgx/4ga0gplALtbHQys5WBCvRKo0iaG2ljWwjyGXeh+VimsPOQvDstBYEg4WooP4hWTCQIneclyAODOws7z47szu7lcpfGzBmZttAtWeYc24AuAfOgfc4jkd6ze4YOAtjSTfe+8XSMDNblfTonBtyzo0CH2FcCtZsNieAF+/97E92u5JOS2cHXAM77VjSc71enyoL2wMu27GZrQFfkg46PWxoZnO5sCAIpSZJMt4xN9MFdijpRNKbmS1lAiVdSFovWo2ZHUmq5QFb4UaLAiVtAg9Jkgx3FQCvRWEda24lLfQFKGkl9GeapoN9AYZ3LqmV2VLAUxAVBUZRNC3pM47jySxgw8w2fpFhA9jPE1TDr+K9Xy4IxMy2ckVRFM1LugvnWcCvMlvm39o3cyjHdd9gv6YAAAAASUVORK5CYII=" />
-            Aplaudir <span>03</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
